230b3cf0df710d85b0968dea1071a25faee3e95d
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-04-29 09:31:38 -0500

    carousel Button components properly positioned

--- src/components/carousel/button/Button.jsx
+++ src/components/carousel/button/Button.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledButton = styled.div`
-  background: rgba(0, 0, 0, 0.3);
-  color: rgba(255, 255, 255, 0.6);
+  background: rgba(0, 0, 0, 0.6);
+  color: rgba(255, 255, 255, 0.7);
   padding: 20px;
   font-size: 1.3rem;
   font-weight: 600;
@@ -12,31 +12,21 @@ const StyledButton = styled.div`
 
   position: absolute;
   bottom: 0;
+  right: ${({ text }) => text === "Next" && "0"};
 
-  color: ${props =>
-    props.text === "Prev" ? "red" : props.text === "Next" ? "blue" : "green"};
-
-  left: ${props => (props.text === "Prev" ? "0" : "auto")};
-  right: ${props => (props.text === "Next" ? "0" : "auto")};
+  &:hover {
+    background: rgba(0, 0, 0, 0.4);
+    color: rgba(255, 255, 255, 1);
+    cursor: pointer;
+  }
 `;
 
-// const LeftButton = styled(StyledButton)`
-//   left: 0;
-// `;
-
-// const RightButton = styled(StyledButton)`
-//   right: 0;
-// `;
-
 const Button = ({ text, handleClick }) => {
-  console.log(text);
-  // return text === "Prev" ? (
-  //   <LeftButton onClick={handleClick}>Prev</LeftButton>
-  // ) : (
-  //   <RightButton onClick={handleClick}>Next</RightButton>
-  // );
-
-  return <StyledButton onClick={handleClick}>{text}</StyledButton>;
+  return (
+    <StyledButton text={text} onClick={handleClick}>
+      {text}
+    </StyledButton>
+  );
 };
 
 Button.propTypes = {
