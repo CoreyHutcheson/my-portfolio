879e36bb24fa1f84572eac72252b9eb6e357dd72
Corey <hutcheson.coreyJ@gmail.com>
2019-05-01 14:12:41 -0500

    continued styling Modal

--- src/components/projects/modal-content/ModalContent.jsx
+++ src/components/projects/modal-content/ModalContent.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { faExternalLinkAlt, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Button from "src/components/button";
+import VisitSiteLink from "./VisitSiteLink";
 
 const StyledModalContent = styled.div`
   font-family: "Raleway", sans-serif;
   padding: 20px;
+  height: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const Title = styled.h2`
@@ -25,22 +29,14 @@ const Tag = styled.div`
 
 const Description = styled.p``;
 
-const StyledLink = styled(Button)`
-  color: #f5f5f5;
-  background: #e31b6d;
+const CloseButton = styled.div`
+  color: rgba(0, 0, 0, 0.7);
+  cursor: pointer;
+  margin-left: auto;
 
   &:hover {
-    background: #f5f5f5;
-    color: #e31b6d;
+    color: rgba(0, 0, 0, 1);
   }
-
-  & > span {
-    margin-left: 10px;
-  }
-`;
-
-const CloseButton = styled.div`
-  cursor: pointer;
 `;
 
 const ModalContent = ({
@@ -50,31 +46,26 @@ const ModalContent = ({
   link = "",
   handleModalClose,
 }) => {
-  const linkElement = (
-    <a
-      style={{ textDecoration: "none" }}
-      href={link}
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <StyledLink>
-        <FontAwesomeIcon icon={faExternalLinkAlt} />
-        <span>Visit Site</span>
-      </StyledLink>
-    </a>
-  );
-
   return (
     <StyledModalContent>
-      <Title>{title}</Title>
-      <Tag>{tag}</Tag>
-      <Description>{description}</Description>
+      <div>
+        <Title>{title}</Title>
+        <Tag>{tag}</Tag>
+        <Description>{description}</Description>
+      </div>
 
-      {link ? linkElement : null}
-
-      <CloseButton onClick={handleModalClose}>
-        <FontAwesomeIcon icon={faTimes} size="2x" />
-      </CloseButton>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {link ? <VisitSiteLink link={link} /> : null}
+        <CloseButton onClick={handleModalClose}>
+          <FontAwesomeIcon icon={faTimes} size="2x" />
+        </CloseButton>
+      </div>
     </StyledModalContent>
   );
 };
