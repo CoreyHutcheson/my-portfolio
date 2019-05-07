f1a75c05c2b6bffbd7a83df8f5a36a4401286c76
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-05-07 18:37:24 -0500

    added ContactForm component to index.jsx page | commented out Portfolio component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import PageWrapper from "src/components/page-wrapper";
-import Projects from "src/components/projects";
+// import Projects from "src/components/projects";
+import ContactForm from "src/components/contact-form";
 
 const IndexPage = () => {
   return (
     <PageWrapper>
-      <Projects />
+      {/* <Projects /> */}
+      <ContactForm />
     </PageWrapper>
   );
 };
