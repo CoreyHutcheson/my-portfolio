d0762590d42e958a559faa97855e67be0bdbb8bb
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-05-07 20:13:24 -0500

    added and styled FormSection in index.jsx page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,46 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
 
 import PageWrapper from "src/components/page-wrapper";
-// import Projects from "src/components/projects";
+import Projects from "src/components/projects";
 import ContactForm from "src/components/contact-form";
+
+const spacer = "4rem";
+
+const FormSection = styled.section`
+  background: #252934;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: ${spacer};
+`;
+
+const StyledSvg = styled.svg`
+  width: 100%;
+  height: 50px;
+  margin-bottom: ${spacer};
+  fill: ${props => props.primaryBackground || "white"};
+`;
 
 const IndexPage = () => {
   return (
-    <PageWrapper>
-      {/* <Projects /> */}
-      <ContactForm />
-    </PageWrapper>
+    <ThemeProvider theme={theme}>
+      <PageWrapper>
+        <Projects />
+
+        <FormSection>
+          <StyledSvg
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <polygon points="0 0, 50 100, 100 0" />
+          </StyledSvg>
+          <ContactForm />
+        </FormSection>
+      </PageWrapper>
+    </ThemeProvider>
   );
 };
 
