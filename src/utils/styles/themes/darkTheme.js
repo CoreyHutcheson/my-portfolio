24afe97f0ad34cd5d074b3d0c3096348a60f95c3
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-05-13 13:11:50 -0500

    updated lightTheme and darkTheme to be more manageable

--- src/utils/styles/themes/darkTheme.js
+++ src/utils/styles/themes/darkTheme.js
@@ -1,20 +1,15 @@
 const darkTheme = {
   name: "dark",
-  color_primary: "#BB86FC",
-  color_primaryAlt: "#3700B3",
-  color_secondary: "#03DAC6",
-  color_background: "#121212",
-  color_backgroundLight: "#383838",
-  color_backgroundDark: "#000000",
-  color_surface: "#121212",
-  color_error: "#CF6679",
-  // Font Colors
-  font_onPrimary1: "rgba(0, 0, 0, 0.87)",
-  font_onPrimary2: "rgba(0, 0, 0, 0.6)",
-  font_onPrimary3: "rgba(0, 0, 0, 0.38)",
-  font_onOther1: "rgba(255, 255, 255, 0.87)",
-  font_onOther2: "rgba(255, 255, 255, 0.6)",
-  font_onOther3: "rgba(255, 255, 255, 0.38)",
+  color_primary: "#121212",
+  color_primaryLight: "#383838",
+  color_primaryDark: "#000000",
+  color_accent: "#ff9800",
+  font_onPrimary1: "rgba(255, 255, 255, 0.87)",
+  font_onPrimary2: "rgba(255, 255, 255, 0.6)",
+  font_onPrimary3: "rgba(255, 255, 255, 0.38)",
+  font_onAccent1: "rgba(0, 0, 0, 0.87)",
+  font_onAccent2: "rgba(0, 0, 0, 0.6)",
+  font_onAccent3: "rgba(0, 0, 0, 0.38)",
 };
 
 export default darkTheme;
--- src/utils/styles/themes/lightTheme.js
+++ src/utils/styles/themes/lightTheme.js
@@ -1,21 +1,15 @@
 const lightTheme = {
   name: "light",
-  color_primary: "#6200EE",
-  color_primaryAlt: "#3700B3",
-  color_secondary: "#03DAC6",
-  color_secondaryAlt: "#018786",
-  color_background: "#eeeeee",
-  color_backgroundLight: "#ffffff",
-  color_backgroundDark: "#cccccc",
-  color_surface: "#ffffff",
-  color_error: "#B00020",
-  // Font Colors
-  font_onPrimary1: "rgba(255, 255, 255, 0.87)",
-  font_onPrimary2: "rgba(255, 255, 255, 0.6)",
-  font_onPrimary3: "rgba(255, 255, 255, 0.38)",
-  font_onOther1: "rgba(0, 0, 0, 0.87)",
-  font_onOther2: "rgba(0, 0, 0, 0.6)",
-  font_onOther3: "rgba(0, 0, 0, 0.38)",
+  color_primary: "#03a9f4",
+  color_primaryLight: "#67daff",
+  color_primaryDark: "#007ac1",
+  color_accent: "#ff9800",
+  font_onPrimary1: "rgba(0, 0, 0, 0.87)",
+  font_onPrimary2: "rgba(0, 0, 0, 0.6)",
+  font_onPrimary3: "rgba(0, 0, 0, 0.38)",
+  font_onAccent1: "rgba(0, 0, 0, 0.87)",
+  font_onAccent2: "rgba(0, 0, 0, 0.6)",
+  font_onAccent3: "rgba(0, 0, 0, 0.38)",
 };
 
 export default lightTheme;
