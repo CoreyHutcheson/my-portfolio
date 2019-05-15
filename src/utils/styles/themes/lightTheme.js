766036ceb4fab1b794b79f350cf84fbc36b0b0da
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-05-14 19:44:38 -0500

    changed lightTheme to new color palette | left comments with previous palette and another palette

--- src/utils/styles/themes/lightTheme.js
+++ src/utils/styles/themes/lightTheme.js
@@ -1,9 +1,34 @@
 const lightTheme = {
   name: "light",
-  color_primary: "#03a9f4",
-  color_primaryLight: "#67daff",
-  color_primaryDark: "#007ac1",
-  color_accent: "#ff9800",
+  /* My created Palette */
+  // color_primary: "#03a9f4",
+  // color_primaryLight: "#67daff",
+  // color_primaryDark: "#007ac1",
+  // color_accent: "#ff9800",
+  // font_onPrimary1: "rgba(0, 0, 0, 0.87)",
+  // font_onPrimary2: "rgba(0, 0, 0, 0.6)",
+  // font_onPrimary3: "rgba(0, 0, 0, 0.38)",
+  // font_onAccent1: "rgba(0, 0, 0, 0.87)",
+  // font_onAccent2: "rgba(0, 0, 0, 0.6)",
+  // font_onAccent3: "rgba(0, 0, 0, 0.38)",
+
+  /* Amber palette */
+  // color_primary: "#ffecb3",
+  // color_primaryLight: "#ffffe5",
+  // color_primaryDark: "#cbba83",
+  // color_accent: "#FF1654",
+  // font_onPrimary1: "rgba(0, 0, 0, 0.87)",
+  // font_onPrimary2: "rgba(0, 0, 0, 0.6)",
+  // font_onPrimary3: "rgba(0, 0, 0, 0.38)",
+  // font_onAccent1: "rgba(0, 0, 0, 0.87)",
+  // font_onAccent2: "rgba(0, 0, 0, 0.6)",
+  // font_onAccent3: "rgba(0, 0, 0, 0.38)",
+
+  /* cOOlers palette */
+  color_primary: "#70C1B3",
+  color_primaryLight: "#B2DBBF",
+  color_primaryDark: "#247BA0",
+  color_accent: "#FF1654",
   font_onPrimary1: "rgba(0, 0, 0, 0.87)",
   font_onPrimary2: "rgba(0, 0, 0, 0.6)",
   font_onPrimary3: "rgba(0, 0, 0, 0.38)",
