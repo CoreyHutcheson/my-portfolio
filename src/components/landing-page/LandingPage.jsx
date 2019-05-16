b58bced09813f4aabfd06e3e276f83941721ebc0
Corey <hutcheson.coreyJ@gmail.com>
2019-05-16 17:19:24 -0500

    calculated height for LandingPage component

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledLandingPage = styled.div`
-  height: 100vh;
-  background: pink;
+  height: calc(
+    ${props => props.theme.landingPageHeight} -
+      ${props => props.theme.navbarHeight}
+  );
+  background: ${props => props.theme.color_primaryDark};
 `;
 
 const LandingPage = () => <StyledLandingPage />;
