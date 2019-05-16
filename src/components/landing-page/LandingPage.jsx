2d9d7527a4002882febb0e62a4a9ebc74a167503
Corey <hutcheson.coreyJ@gmail.com>
2019-05-16 01:53:41 -0500

    created basic LandingPage component

---
+++ src/components/landing-page/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLandingPage = styled.div`
+  height: 100vh;
+  background: pink;
+`;
+
+const LandingPage = () => <StyledLandingPage />;
+
+export default LandingPage;
