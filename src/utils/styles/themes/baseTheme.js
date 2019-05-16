20c3364a5d6e5c69e560c99b62dc370935181ba0
Corey <hutcheson.coreyJ@gmail.com>
2019-05-16 17:18:18 -0500

    added landingPageHeight and navbarHeight to baseTheme

--- src/utils/styles/themes/baseTheme.js
+++ src/utils/styles/themes/baseTheme.js
@@ -2,6 +2,8 @@ const baseTheme = {
   flexCenterMixin: `display: flex; justify-content: center; align-items: center;`,
   sectionPadding: "10rem",
   sectionBorderSpacer: "2rem",
+  landingPageHeight: "100vh",
+  navbarHeight: "50px",
 };
 
 export default baseTheme;
