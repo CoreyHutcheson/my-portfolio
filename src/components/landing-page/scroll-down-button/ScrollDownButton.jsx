9a84006dfc5bffe272b77c52e529802ce9447b3d
Corey <hutcheson.coreyJ@gmail.com>
2019-05-17 22:31:06 -0500

    set ScrollDownButton component to link to #navbar

--- src/components/landing-page/scroll-down-button/ScrollDownButton.jsx
+++ src/components/landing-page/scroll-down-button/ScrollDownButton.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Icon = styled(FontAwesomeIcon)`
+  transition: 0.3s ease;
+`;
+
+const StyledButton = styled(Link)`
+  width: 60px;
+  height: 50px;
+  background: green;
+  color: inherit;
+  ${props => props.theme.flexCenterMixin};
+  cursor: pointer;
+  text-decoration: none;
+
+  :hover {
+    ${Icon} {
+      transform: rotate(90deg);
+    }
+  }
+`;
 
 const ScrollDownButton = () => (
-  
+  <StyledButton to="/#navbar">
+    <Icon icon={faArrowRight} size="2x" />
+  </StyledButton>
 );
+
+ScrollDownButton.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default ScrollDownButton;
