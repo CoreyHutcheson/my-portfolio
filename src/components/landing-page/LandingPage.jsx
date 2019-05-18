fe5aa86e65b5b31c58fa7bf27bc82cfb44832922
Corey <hutcheson.coreyJ@gmail.com>
2019-05-17 22:32:32 -0500

    added ScrollDownButton to LandingPage component

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+import ScrollDownButton from "./scroll-down-button";
+
 const StyledLandingPage = styled.div`
   height: ${props =>
     `calc(${props.theme.landingPageHeight} - ${props.theme.navbarHeight})`};
   background: ${props => props.theme.color_primaryDark};
 `;
 
-const LandingPage = () => <StyledLandingPage />;
+const LandingPage = () => {
+  return (
+    <StyledLandingPage>
+      <ScrollDownButton />
+    </StyledLandingPage>
+  );
+};
 
 export default LandingPage;
