ef311e1b0444e68c556aa9bce494374d7fd1afe1
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-05-28 18:14:14 -0500

    Add boxShadow and borderRadius to baseTheme

--- src/utils/styles/themes/baseTheme.js
+++ src/utils/styles/themes/baseTheme.js
@@ -4,6 +4,8 @@ const baseTheme = {
   sectionBorderSpacer: "2rem",
   landingPageHeight: "100vh",
   navbarHeight: "50px",
+  boxShadow: "3px 3px 7px 0px rgba(0, 0, 0, 0.75);",
+  borderRadius: "5px",
 };
 
 export default baseTheme;
