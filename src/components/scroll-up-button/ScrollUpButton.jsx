23a70c3995c0ef5c9a7a2be943639aa4dc6f6598
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-06-01 12:52:25 -0500

    Revert "Hides ScrollUpButton component near bottom of page"
    This reverts commit d90545607f61a9e7156a973836bea976528203db.

--- src/components/scroll-up-button/ScrollUpButton.jsx
+++ src/components/scroll-up-button/ScrollUpButton.jsx
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
 import { withTheme } from "styled-components";
 import ScrollButton from "react-scroll-up-button";
 import { vhToPixels } from "src/utils/js/usePinStartHeight.js";
 
-const Container = styled.div`
-  display: ${props => (props.show ? "block" : "none")};
-`;
-
 const ScrollUpButton = props => {
-  const [showButton, setShowButton] = useState(true);
-
-  const handleScroll = () => {
-    let hideHeight = document.body.clientHeight - window.innerHeight - 90;
-    let scrollHeight = window.scrollY;
-
-    if (scrollHeight > hideHeight) {
-      setShowButton(false);
-    } else {
-      setShowButton(true);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  });
-
   const styles = {
     color: props.theme.font_onPrimary1,
   };
 
-  return (
-    <Container show={showButton}>
-      <ScrollButton ShowAtPosition={vhToPixels(100) - 51} style={styles} />
-    </Container>
-  );
+  return <ScrollButton ShowAtPosition={vhToPixels(100) - 51} style={styles} />;
 };
 
 export default withTheme(ScrollUpButton);
