d90545607f61a9e7156a973836bea976528203db
Corey Hutcheson <hutcheson.coreyj@gmail.com>
2019-06-01 11:15:33 -0500

    Hides ScrollUpButton component near bottom of page
    Adds an wrapper around the ScrollUpButton component that adds an event
    listener to the scroll event. When the top of the footer appears, the
    ScrollUpButton component disappears, allowing for clear view of the footer
    and corresponding footer scrollupbutton.

--- src/components/scroll-up-button/ScrollUpButton.jsx
+++ src/components/scroll-up-button/ScrollUpButton.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import { withTheme } from "styled-components";
 import ScrollButton from "react-scroll-up-button";
 import { vhToPixels } from "src/utils/js/usePinStartHeight.js";
 
+const Container = styled.div`
+  display: ${props => (props.show ? "block" : "none")};
+`;
+
 const ScrollUpButton = props => {
+  const [showButton, setShowButton] = useState(true);
+
+  const handleScroll = () => {
+    let hideHeight = document.body.clientHeight - window.innerHeight - 90;
+    let scrollHeight = window.scrollY;
+
+    if (scrollHeight > hideHeight) {
+      setShowButton(false);
+    } else {
+      setShowButton(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   const styles = {
     color: props.theme.font_onPrimary1,
   };
 
-  return <ScrollButton ShowAtPosition={vhToPixels(100) - 51} style={styles} />;
+  return (
+    <Container show={showButton}>
+      <ScrollButton ShowAtPosition={vhToPixels(100) - 51} style={styles} />
+    </Container>
+  );
 };
 
 export default withTheme(ScrollUpButton);
