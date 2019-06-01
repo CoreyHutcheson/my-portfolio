e9fb5e19af92a2094c7496f22fb8bbe27d0d939a
Corey <hutcheson.coreyJ@gmail.com>
2019-06-01 00:49:41 -0500

    Adds icons to Footer components and began styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebook,
+  faCodepen,
+} from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => <footer>Footer</footer>;
+const StyledFooter = styled.footer`
+  height: 90px;
+  background: ${props => props.theme.color_primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconContainer = styled.div`
+  max-width: 40%;
+  width: 40%;
+  min-width: 250px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)``;
+
+const Link = styled.a`
+  background: ${props => props.theme.color_primaryDark};
+  color: ${props => props.theme.font_onPrimary1};
+  padding: 0.5rem;
+  text-decoration: none;
+  overflow: hidden;
+
+  &:hover {
+    background: ${props => props.theme.color_accent};
+    color: ${props => props.theme.font_onAccent1};
+
+    & ${StyledIcon} {
+      animation: slideDown 0.1s;
+
+      @keyframes slideDown {
+        0% {
+          transform: translatex(-100px);
+        }
+      }
+    }
+  }
+`;
+
+const Footer = () => (
+  <StyledFooter>
+    <IconContainer>
+      <Link href="#0">
+        <StyledIcon icon={faGithub} size="3x" />
+      </Link>
+
+      <Link href="#0">
+        <StyledIcon icon={faFacebook} size="3x" />
+      </Link>
+
+      <Link href="#0">
+        <StyledIcon icon={faCodepen} size="3x" />
+      </Link>
+    </IconContainer>
+  </StyledFooter>
+);
 
 export default Footer;
