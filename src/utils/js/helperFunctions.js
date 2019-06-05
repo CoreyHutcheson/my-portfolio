1166423be439931f735f106b6d1f921abbba8c7e
Corey <hutcheson.coreyJ@gmail.com>
2019-06-05 01:23:38 -0500

    Creates helperFunction file inside utils and moves used functions there

---
+++ src/utils/js/helperFunctions.js
@@ -0,0 +1,45 @@
+import messages from "src/data/contactMessages.js";
+
+export const getRandomMessage = () => {
+  const randomNumber = (min, max) => {
+    return (
+      Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) +
+      Math.ceil(min)
+    );
+  };
+
+  return messages[randomNumber(0, messages.length - 1)];
+};
+
+export const scrollTo = pos => {
+  window.scrollTo({
+    top: pos,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+export const sortObjects = (obj, sortBy) => {
+  return obj.sort((a, b) => {
+    let propA = a[sortBy].toUpperCase();
+    let propB = b[sortBy].toUpperCase();
+
+    return propA < propB ? -1 : propA > propB ? 1 : 0;
+  });
+};
+
+export const getTitle = iconName => {
+  const replacements = {
+    code: "jsx",
+  };
+
+  return replacements.hasOwnProperty(iconName)
+    ? replacements[iconName]
+    : iconName;
+};
+
+export function vhToPixels(vh) {
+  if (typeof window !== `undefined`) {
+    return Math.round(window.innerHeight / (100 / vh));
+  }
+}
--- src/utils/js/scrollTo.js
+++
@@ -1,9 +0,0 @@
-const scrollTo = pos => {
-  window.scrollTo({
-    top: pos,
-    left: 0,
-    behavior: "smooth",
-  });
-};
-
-export default scrollTo;
