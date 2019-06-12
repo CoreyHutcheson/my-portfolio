3c432082d887dbbefd9277a7b796099c7f701184
Corey <hutcheson.coreyJ@gmail.com>
2019-06-12 18:15:40 -0500

    Remove facebook footerLink, and move links to own file inside data folder

---
+++ src/data/footerLinks.js
@@ -0,0 +1,14 @@
+import { faGithub, faCodepen } from "@fortawesome/free-brands-svg-icons";
+
+export const footerLinks = [
+  {
+    icon: faGithub,
+    href: "https://github.com/CoreyHutcheson",
+    ariaLabel: "Corey's Github",
+  },
+  {
+    icon: faCodepen,
+    href: "https://codepen.io/CoreyHutcheson/",
+    ariaLabel: "Corey's Codepen",
+  },
+];
