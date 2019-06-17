2f127e4a5e5d03e2d6fc234432f80d6bfa460d3c
Corey <hutcheson.coreyJ@gmail.com>
2019-06-16 20:58:33 -0500

    Use new Navbar component inside PageWrapper component

--- src/components/page-wrapper/PageWrapper.jsx
+++ src/components/page-wrapper/PageWrapper.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import LandingPage from "src/components/landing-page";
-import NavBar from "src/components/nav-bar";
+import { Navbar } from "src/components/nav-bar";
 import Footer from "src/components/footer";
+import { faChessKing } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PageWrapper = ({ children, handleThemeChange }) => (
   <>
     <header id="navbar">
-      <NavBar handleThemeChange={handleThemeChange} />
+      <Navbar
+        icon={<FontAwesomeIcon icon={faChessKing} size="2x" />}
+        handleThemeChange={handleThemeChange}
+      >
+        <div element="about" text="About" />
+        <div element="projects" text="Projects" />
+        <div element="contact" text="Contact" />
+      </Navbar>
     </header>
 
     <main>
