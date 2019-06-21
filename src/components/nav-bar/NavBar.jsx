c94ce4ffcc556ccdd1acae4d936a86f1149344e5
Corey <hutcheson.coreyJ@gmail.com>
2019-06-20 23:52:47 -0500

    Call newly imported reusable-react navbar

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,125 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Scrollspy from "react-scrollspy";
-import Headroom from "react-headroom";
+import { Navbar as ReusableNavbar } from "@corey_hutcheson/reusable-react";
 
-import { ToggleButton } from "./ToggleButton";
-import { Link } from "./Link";
+import { faSun, faMoon, faChessKing } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import SliderButton from "src/components/slider-button";
 
-const Header = styled.header`
-  background: ${props => props.theme.color_primary};
-  position: relative;
-  width: 100%;
-  min-height: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const StyledIcon = styled.div`
-  color: ${props => props.theme.font_onPrimary1};
-  margin-left: ${props => props.theme.margin};
-`;
-
-const StyledToggle = styled(ToggleButton)`
-  margin-left: auto;
-  margin-right: ${props => props.theme.margin};
-
-  @media (min-width: 600px) {
-    display: none;
-  }
-`;
-
-const LinkContainer = styled(Scrollspy)`
-  background: ${props => props.theme.color_primaryDark};
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  position: absolute;
-  top: 50px;
-  transition: transform 0.3s ease-out;
-  transform: translateX(${props => (props.open ? 0 : "-100%")});
-
-  @media (min-width: 600px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-
-    margin-left: ${props => props.theme.margin};
-    background: inherit;
-    position: relative;
-    top: 0;
-    transition: none;
-    transform: translateX(0%);
-  }
-`;
-
-const Slider = styled(SliderButton)`
-  @media (min-width: 600px) {
-    margin-left: auto;
-    margin-right: ${props => props.theme.margin};
-  }
-`;
-
 export const Navbar = props => {
-  const {
-    icon,
-    scrollToOffset,
-    children,
-    className,
-    handleThemeChange,
-  } = props;
-  const [open, setOpen] = useState(false);
+  const { className, handleThemeChange } = props;
 
-  const handleTogglerClick = () => {
-    setOpen(!open);
-  };
-
-  const handleLinkClick = () => {
-    setOpen(false);
-  };
-
-  const Links = children.map(({ props }) => (
-    <Link
-      key={props.element}
-      element={props.element}
-      handleLinkClick={handleLinkClick}
-      scrollToOffset={scrollToOffset}
-    >
-      {props.text}
-    </Link>
-  ));
+  const links = [
+    {
+      element: "about",
+      text: "About",
+    },
+    {
+      element: "projects",
+      text: "Projects",
+    },
+    {
+      element: "contact",
+      text: "Contact",
+    },
+  ];
 
   return (
-    <Headroom>
-      <Header className={className}>
-        {/* Include Icon Component if passed in via props */}
-        {icon && <StyledIcon>{icon}</StyledIcon>}
-
-        <StyledToggle open={open} handleClick={handleTogglerClick} />
-
-        <LinkContainer
-          open={open}
-          items={["about", "projects", "contact"]}
-          currentClassName="is-current"
-          componentTag="nav"
-          offset={-300}
-        >
-          {Links}
-
-          <Slider
-            handleChange={handleThemeChange}
-            preText={<FontAwesomeIcon icon={faSun} />}
-            postText={<FontAwesomeIcon icon={faMoon} />}
-          />
-        </LinkContainer>
-      </Header>
-    </Headroom>
+    <ReusableNavbar
+      links={links}
+      icon={<FontAwesomeIcon icon={faChessKing} size="2x" />}
+      extraComp={
+        <SliderButton
+          handleChange={handleThemeChange}
+          preText={<FontAwesomeIcon icon={faSun} />}
+          postText={<FontAwesomeIcon icon={faMoon} />}
+        />
+      }
+      className={className}
+    />
   );
 };
 
@@ -129,10 +47,6 @@ Navbar.defaultProps = {
 };
 
 Navbar.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.bool, PropTypes.element]),
-  dark: PropTypes.bool,
-  scrollOffset: PropTypes.number,
-  children: PropTypes.node.isRequired,
   className: PropTypes.string,
   handleThemeChange: PropTypes.func,
 };
--- src/components/page-wrapper/PageWrapper.jsx
+++ src/components/page-wrapper/PageWrapper.jsx
@@ -4,20 +4,11 @@ import PropTypes from "prop-types";
 import LandingPage from "src/components/landing-page";
 import { Navbar } from "src/components/nav-bar";
 import Footer from "src/components/footer";
-import { faChessKing } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PageWrapper = ({ children, handleThemeChange }) => (
   <>
     <header id="navbar">
-      <Navbar
-        icon={<FontAwesomeIcon icon={faChessKing} size="2x" />}
-        handleThemeChange={handleThemeChange}
-      >
-        <div element="about" text="About" />
-        <div element="projects" text="Projects" />
-        <div element="contact" text="Contact" />
-      </Navbar>
+      <Navbar handleThemeChange={handleThemeChange} />
     </header>
 
     <main>
